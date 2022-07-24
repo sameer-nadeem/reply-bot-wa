257e51dd2ce0db290145ad3b81e6859db44a06ce
Muhammad Sameer Nadeem <sameer.nadeem24@gmail.com>
2022-07-25 01:30:28 +0500

    fix

--- messageHandler.js
+++ messageHandler.js
@@ -2,7 +2,9 @@ const { Client, LocalAuth } = require("whatsapp-web.js");
 const logger = require("./logger");
 const path = require("path");
 const client = new Client({
-  authStrategy: new LocalAuth({ dataPath: path.join(__dirname, "/session") }),
+  authStrategy: new LocalAuth({
+    dataPath: path.join(process.cwd(), "/session"),
+  }),
 });
 const cron = require("node-cron");
 
