0c5129e670df4059c60687ccda6a83c3988a5252
Muhammad Sameer Nadeem <sameer.nadeem24@gmail.com>
2022-07-25 01:12:39 +0500

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//index.js code for integrating Google Calendar
-
+const qrcode = require("qrcode-terminal");
 const credentials = require("./credentials.json");
 const axios = require("axios");
 const moment = require("moment");
@@ -7,6 +6,7 @@ const GOOGLE_CALENDAR_API_KEY = credentials.api_key;
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cron = require("node-cron");
 const getPublicHolidays = async () => {
   const { data } = await axios.get(
     `https://www.googleapis.com/calendar/v3/calendars/en.pk%23holiday%40group.v.calendar.google.com/events?key=${GOOGLE_CALENDAR_API_KEY}`
@@ -19,9 +19,14 @@ const getPublicHolidays = async () => {
 
   return publicHolidays;
 };
-
-// require("./messageHandler");
-
-app.get("/", (req, res) => res.send("hello world"));
+let qrCodeStr = "yet-to-be-generated";
+const createClient = require("./messageHandler");
+const client = createClient();
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+  qrCodeStr = qr;
+});
+client.initialize();
+app.get("/", (req, res) => res.send(qrCodeStr));
 
 app.listen(PORT);
